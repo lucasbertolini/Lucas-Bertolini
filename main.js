e89d4ab074d5c341ac46f94fb08be361fd3e0246
Lucas Bertolini <42697227+lucasbertolini@users.noreply.github.com>
2022-04-18 09:33:21 -0300

    mobile features

--- main.js
+++ main.js
@@ -1,7 +1,26 @@
+
+
+let sobre = document.querySelector('#sobre');
+let contato = document.querySelector('#contato');
+let projetos = document.querySelector('#projetos');
+let topBtn = document.querySelector('#top-btn');
 let data_info;
 getApi();
 
+window.onscroll = () =>{
+    if( document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
+        topBtn.style.display = 'block';
+    }else{
+        topBtn.style.display = 'none'
+    }
+}
 
+topBtn.addEventListener('click', () => {
+
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0
+
+})
 function getApi(){
 
     const API_TOKEN = '4be5b394c384ad9199396a5bc40dfb42aa43b5aeff3e911474dd59f9374362ed';
@@ -28,3 +47,12 @@ function rodar(){
 
 }
 
+sobre.addEventListener('click', () => {
+    document.querySelector('.sobre').scrollIntoView();
+})
+projetos.addEventListener('click', () => {
+    document.querySelector('.projetos').scrollIntoView();
+});
+contato.addEventListener('click', () => {
+    document.querySelector('footer').scrollIntoView();
+})
